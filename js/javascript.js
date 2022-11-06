00b18183ddf4f42017aa6cd0258af34eeb7c1b1f
Benzlot <73481640+Benzlot@users.noreply.github.com>
2022-11-06 23:32:38 +0700

    Main page and login completed

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,4 @@
+const Navrela = document.getElementById("relative");
 const Navbar = document.getElementById("fixed");
 
 document.addEventListener("scroll",()=>{
@@ -6,3 +7,23 @@ document.addEventListener("scroll",()=>{
         Navbar.classList.replace("fixdown","unfixdown"); 
     };
 });
+
+Navbar.addEventListener("mouseover",()=>{
+    Navbar.classList.replace("brown","browntran");
+    Navrela.classList.replace("brown","browntran");
+})
+
+Navrela.addEventListener("mouseover",()=>{
+    Navbar.classList.replace("brown","browntran");
+    Navrela.classList.replace("brown","browntran");
+})
+
+Navbar.addEventListener("mouseout",()=>{
+    Navbar.classList.replace("browntran","brown");
+    Navrela.classList.replace("browntran","brown");
+})
+
+Navrela.addEventListener("mouseout",()=>{
+    Navbar.classList.replace("browntran","brown");
+    Navrela.classList.replace("browntran","brown");
+})
